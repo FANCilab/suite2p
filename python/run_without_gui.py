e1c67d30945633169f70ea2c6a5643a02a63dfdc
heatherstrelevitz <164369264+heatherstrelevitz@users.noreply.github.com>
2025-09-30 12:32:12 +0200

    Update run_without_gui.py

--- python/run_without_gui.py
+++ python/run_without_gui.py
@@ -8,9 +8,10 @@ from suite2p.run_s2p import run_s2p
 ops =  np.load("file_path", allow_pickle=True)
 ops = ops.item()
 db = {
-    'data_path': "path",
+    'data_path': ["path"],
     'save_path0': "path",
 }
 
 
 output_ops = suite2p.run_s2p(ops=ops, db=db)
+
